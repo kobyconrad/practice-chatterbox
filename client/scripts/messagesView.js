bec174d47deed86576a2f102577646208ff781a1
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-03-17 12:30:05 -0700

    added work on events

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -4,15 +4,25 @@ var MessagesView = {
   initialize: function() {},
 
   // builds out the message, turn it into HTML
-  render: function(message) {
+  render: function(message, className = "") {
     // render the message
-    return (htmlMessage = $("<p></p>").text(`${message.text}`));
+    if (className.length > 1) {
+      var currentClass = `className = "${className}"`;
+      return (htmlMessage = $(`<p ${currentClass}></p>`).text(`${message}`));
+    } else {
+      return (htmlMessage = $(`<p></p>`).text(`${message}`));
+    }
   },
 
   // adds it to the DOM
   renderMessage: function(message) {
     // take my render, and add it to #chats
-    var currentMsg = MessagesView.render(message);
+    var currentRoom = MessagesView.render(message.roomname);
+    var currentUsername = MessagesView.render(message.username, "username");
+    var currentMsg = MessagesView.render(message.text);
+
+    $("#chats").append(currentRoom);
+    $("#chats").append(currentUsername);
     $("#chats").append(currentMsg);
   }
 };
